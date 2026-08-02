edace3659666960da3e354fb2a42dc6f1edd90fa
Gnana Venkata Reddy Datla <dvenkatareddy2006@gmail.com>
2026-08-02 10:07:32 +0530

    Time: 378 ms (40.34%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -2,5 +2,5 @@
 SELECT e.name
 FROM Employee e
 JOIN Employee m ON e.id = m.managerID
-GROUP BY e.id, e.name
+GROUP BY e.id, e.name -- e.name is included in GROUP BY for MySQL ONLY_FULL_GROUP_BY strict compatibility, even though e.id is unique. If it id.
 HAVING COUNT(m.id) >= 5;
